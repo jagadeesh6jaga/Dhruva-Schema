832cca2afdc0db07603e875f9017ab7372cb9fa4
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-03-30 02:22:17 +0530

    Added create snapshot request

--- services/request/__init__.py
+++ services/request/__init__.py
@@ -1,15 +1,28 @@
-from .ulca_inference_query import ULCAInferenceQuery
-from .service_view_request import ServiceViewRequest
-from .ulca_generic_inference_request import ULCAGenericInferenceRequest
-from .ulca_translation_inference_request import ULCATranslationInferenceRequest, _ULCATranslationInferenceRequestConfig
-from .ulca_asr_inference_request import ULCAAsrInferenceRequest, _ULCAAsrInferenceRequestConfig
-from .ulca_tts_inference_request import ULCATtsInferenceRequest, _ULCATtsInferenceRequestConfig
-from .ulca_ner_inference_request import ULCANerInferenceRequest, _ULCANerInferenceRequestConfig
-from .ulca_s2s_inference_request import ULCAS2SInferenceRequest
-from .service_create_request import ServiceCreateRequest
-from .ulca_pipeline_inference_request import ULCAPipelineInferenceRequest
-from .model_create_request import ModelCreateRequest
-from .model_view_request import ModelViewRequest
-from .service_update_request import ServiceUpdateRequest
-from .model_update_request import ModelUpdateRequest
+from .create_snapshot_request import CreateSnapshotRequest
 from .feedback_submit_request import FeedbackSubmitRequest
+from .model_create_request import ModelCreateRequest
+from .model_update_request import ModelUpdateRequest
+from .model_view_request import ModelViewRequest
+from .service_create_request import ServiceCreateRequest
+from .service_update_request import ServiceUpdateRequest
+from .service_view_request import ServiceViewRequest
+from .ulca_asr_inference_request import (
+    ULCAAsrInferenceRequest,
+    _ULCAAsrInferenceRequestConfig,
+)
+from .ulca_generic_inference_request import ULCAGenericInferenceRequest
+from .ulca_inference_query import ULCAInferenceQuery
+from .ulca_ner_inference_request import (
+    ULCANerInferenceRequest,
+    _ULCANerInferenceRequestConfig,
+)
+from .ulca_pipeline_inference_request import ULCAPipelineInferenceRequest
+from .ulca_s2s_inference_request import ULCAS2SInferenceRequest
+from .ulca_translation_inference_request import (
+    ULCATranslationInferenceRequest,
+    _ULCATranslationInferenceRequestConfig,
+)
+from .ulca_tts_inference_request import (
+    ULCATtsInferenceRequest,
+    _ULCATtsInferenceRequestConfig,
+)
