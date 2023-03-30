096373b448b715743d8d20279c0c3d7009a44b46
Rugz007 <dsweb007@gmail.com>
2023-03-30 09:54:45 +0530

    Edited Feedback request schema

--- services/request/feedback_submit_request.py
+++ services/request/feedback_submit_request.py
@@ -1,7 +1,12 @@
+from typing import Optional
 from pydantic import BaseModel
 
+class _LanguagePair(BaseModel):
+    sourceLanguage: str
+    targetLanguage: Optional[str]
+
 class FeedbackSubmitRequest(BaseModel):
-    language: str
+    language: _LanguagePair
     example: str
     rating: int
     comments: str
