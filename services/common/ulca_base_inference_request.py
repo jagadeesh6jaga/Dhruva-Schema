b9a024c266d185941ee191aef3b53b9a8a5ac168
Gokul NC <gokulnc@ymail.com>
2023-04-21 16:52:59 +0530

    Make `controlConfig` optional

--- services/common/ulca_base_inference_request.py
+++ services/common/ulca_base_inference_request.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 from .ulca_control_config import _ControlConfig
 
 
 class _ULCABaseInferenceRequest(BaseModel):
-    controlConfig: _ControlConfig
+    controlConfig: Optional[_ControlConfig]
