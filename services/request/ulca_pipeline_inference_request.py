97480973e66d0a7521734d63f9d5c92941799e13
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-04-21 14:42:25 +0530

    Added back import

--- services/request/ulca_pipeline_inference_request.py
+++ services/request/ulca_pipeline_inference_request.py
@@ -3,7 +3,8 @@ from pydantic import BaseModel
 from ..common import _ULCABaseInferenceRequest, _ULCATaskType
 
 # from .ulca_asr_inference_request import _ULCAAsrInferenceRequestConfig
-# from .ulca_generic_inference_request import ULCAGenericInferenceRequestWithoutConfig
+from .ulca_generic_inference_request import ULCAGenericInferenceRequestWithoutConfig
+
 # from .ulca_translation_inference_request import _ULCATranslationInferenceRequestConfig
 # from .ulca_tts_inference_request import _ULCATtsInferenceRequestConfig
 
