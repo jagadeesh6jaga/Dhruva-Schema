4d80076885a6b8428d444919f746adf9bb0b3287
Rugz007 <dsweb007@gmail.com>
2023-04-26 15:26:54 +0530

    Configuration options optional in TTS

--- services/common/ulca_tts_inference_config.py
+++ services/common/ulca_tts_inference_config.py
@@ -1,8 +1,8 @@
-from typing import Literal
+from typing import Literal, Optional
 from .ulca_base_monolingual_config import _ULCABaseMonolingualTaskConfig
 
 
 class _ULCATtsInferenceConfig(_ULCABaseMonolingualTaskConfig):
     gender: Literal["male", "female"]
-    samplingRate: int
-    audioFormat: Literal["wav", "mp3", "flac", "pcm", "sph"]
+    samplingRate: Optional[int]
+    audioFormat: Optional[Literal["wav", "mp3", "flac", "pcm", "sph"]]
