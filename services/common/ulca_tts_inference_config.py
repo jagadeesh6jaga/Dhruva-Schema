8720b07fa817170ebf86b21b9bf2631890870e8c
Rugz007 <dsweb007@gmail.com>
2023-04-26 08:15:23 +0530

    Updated TTS schema

--- services/common/ulca_tts_inference_config.py
+++ services/common/ulca_tts_inference_config.py
@@ -3,4 +3,6 @@ from .ulca_base_monolingual_config import _ULCABaseMonolingualTaskConfig
 
 
 class _ULCATtsInferenceConfig(_ULCABaseMonolingualTaskConfig):
-    gender: Literal['male', 'female']
+    gender: Literal["male", "female"]
+    samplingRate: int
+    audioFormat: Literal["wav", "mp3", "flac", "pcm", "sph"]
