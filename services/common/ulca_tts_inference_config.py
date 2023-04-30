ca4913d59b325b73dccdceed2675a279dbfe1b23
Rugz007 <dsweb007@gmail.com>
2023-04-30 09:03:20 +0530

    added default values for tts config

--- services/common/ulca_tts_inference_config.py
+++ services/common/ulca_tts_inference_config.py
@@ -4,5 +4,5 @@ from .ulca_base_monolingual_config import _ULCABaseMonolingualTaskConfig
 
 class _ULCATtsInferenceConfig(_ULCABaseMonolingualTaskConfig):
     gender: Literal["male", "female"]
-    samplingRate: Optional[int]
-    audioFormat: Optional[Literal["wav", "mp3", "flac", "pcm", "flv","ogg"]]
+    samplingRate: Optional[int] = 22050
+    audioFormat: Optional[Literal["wav", "mp3", "flac", "pcm", "flv","ogg"]] = "wav"
